43f220aa6743e136642ee5491b0722953dbe1336
sevenkinako <sevenkinako.0609@gmail.com>
2025-09-27 22:15:00 +0900

    PHP 005課題提出

---
+++ kadai_005/score.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP課題</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      $score = 80+ 60 + 55 + 40 + 100 + 25 + 80 + 95 + 30 + 60;
+      echo $score /10;
+      ?>
+    </p>
+  </body>
+
+</html>
