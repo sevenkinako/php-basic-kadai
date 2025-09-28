8f1ab42d42bb16810864ca70a5bb47e3aed2ee12
sevenkinako <sevenkinako.0609@gmail.com>
2025-09-28 17:18:57 +0900

    PHP007課題提出

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="UTF-8">
+    <title>PHP007課題</title>
+  </head>
+  <body>
+    <p>
+      <?php
+      $vegetable =['name' => 'onion', 'price' => 200, 'weight' => 160];
+      print_r($vegetable);
+      ?>
+    </p>
+  </body>
+
+</html>
